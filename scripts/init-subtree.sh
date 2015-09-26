89be3552ddaf6b51175b5fd3ad615bc962dc3c19
innyso <mini.inny@gmail.com>
2015-09-26 22:53:59 +1000

    Update init subtree script to add extra subtrees

--- scripts/init-subtree.sh
+++ scripts/init-subtree.sh
@@ -1,9 +1,15 @@
  #!/bin/bash
 
-remote_exist=`git branch -r | grep shared-ansible`
+remote_exist=`git remote show | grep shared-ansible`
 
 if [ -z "$remote_exist" ]; then
   git remote add shared-ansible https://github.com/innyso/ansible.git
 fi
 
-git subtree add --prefix=mysetup/ansible --squash shared-ansible master
+prefix=( "mysetup/ansible" "aptly/ansible")
+for i in "${prefix[@]}"
+do
+  if [ ! -d "$i" ]; then
+    git subtree add --prefix=$i --squash shared-ansible master
+  fi
+done
